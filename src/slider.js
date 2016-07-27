e95f8f50eba3360a8710be0e44033041f5896d8c
RyanScovilleEpages <r.scoville@epages.com>
2016-07-27 18:00:18 +0200

    Lazy loading
    - added lazy loading

--- src/slider.js
+++ src/slider.js
@@ -7,9 +7,11 @@ class Slider extends React.Component {
     super();
     this.onPrev = this.onPrev.bind(this);
     this.onNext = this.onNext.bind(this);
+    this.concatToLazyLoadedArray = this.concatToLazyLoadedArray.bind(this);
 
     this.state = {
-      activeIndex: props.data.initialActiveIndex
+      activeIndex: props.data.initialActiveIndex,
+      lazyLoadedArray: [props.data.initialActiveIndex]
     }
   }
 
@@ -22,14 +24,16 @@ class Slider extends React.Component {
       images: [],
       slideInterval: 2500,
       showBullets: true,
-      initialActiveIndex: 0
+      initialActiveIndex: 0,
+      lazyLoadImages: 1
     }
   }
 
   onPrev() {
     let prevIndex = (this.state.activeIndex - 1 >= 0) ? this.state.activeIndex - 1 : 0
     this.setState({
-      activeIndex: prevIndex
+      activeIndex: prevIndex,
+      lazyLoadedArray: this.concatToLazyLoadedArray(prevIndex)
     })
   }
 
@@ -38,8 +42,14 @@ class Slider extends React.Component {
       ? this.state.activeIndex + 1
       : this.props.data.images.length -1
     this.setState({
-      activeIndex: nextIndex
+      activeIndex: nextIndex,
+      lazyLoadedArray: this.concatToLazyLoadedArray(nextIndex)
     })
+  }
+
+  concatToLazyLoadedArray(index) {
+    let lazyLoaded = this.state.lazyLoadedArray
+    return (lazyLoaded.indexOf(index) === -1) ? lazyLoaded.concat(index) : lazyLoaded
   }
 
   render() {
@@ -47,10 +57,14 @@ class Slider extends React.Component {
     return (
       <div className="slider-wrapper">
         <div className="prev" onClick={() => this.onPrev()}>-</div>
-        <div className="slider" style={{left: -(this.state.activeIndex * 100) + '%', transition: 'left ' + data.slideInterval / 1000 + 's'}}>
+        <div className="slider" style={{left: -(this.state.activeIndex * 100) + '%', transition: 'left ' + (data.slideInterval / 1000) + 's'}}>
           {data.images.map((image, index) =>
             <div className="slide" key={'image_'+index}>
-              <div ref={'slide_'+index} key={'slide_'+index} className="slide-inner" style={{backgroundImage: 'url('+image+')'}}/>
+              <div key={'slide_'+index}
+                   className="slide-inner"
+                   data-image={image}
+                   style={{backgroundImage: 'url(' + ((this.state.lazyLoadedArray.indexOf(index) === -1) ? '/src/img/spinner.gif' : image) + ')'}}
+              />
             </div>
           )}
         </div>
@@ -59,5 +73,8 @@ class Slider extends React.Component {
     )
   }
 }
+
+// <div ref={'slide_'+index} key={'slide_'+index} className="slide-inner" style={{backgroundImage: 'url('+image+')'}}/>
+
 
 export default Slider;
