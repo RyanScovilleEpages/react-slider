fbcb0947cb943b838f8832e9c1dc8f2cea7bd066
RyanScovilleEpages <r.scoville@epages.com>
2016-07-28 11:45:20 +0200

    Follow finger swiping
    - currently non-functional

--- src/slider.js
+++ src/slider.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './css/slider.css';
+import Swipeable from 'react-swipeable';
 
 class Slider extends React.Component {
 
@@ -8,10 +10,14 @@ class Slider extends React.Component {
     this.onPrev = this.onPrev.bind(this);
     this.onNext = this.onNext.bind(this);
     this.concatToLazyLoadedArray = this.concatToLazyLoadedArray.bind(this);
+    this.onSwiping = this.onSwiping.bind(this);
 
     this.state = {
       activeIndex: props.data.initialActiveIndex,
-      lazyLoadedArray: [props.data.initialActiveIndex]
+      lazyLoadedArray: [props.data.initialActiveIndex],
+      dX: 0,
+      sliderWrapperWidth: 0,
+      imagesLength: 0
     }
   }
 
@@ -29,8 +35,17 @@ class Slider extends React.Component {
     }
   }
 
+  componentDidMount() {
+    // find the width of the swiper wrapper
+    this.setState({sliderWrapperWidth: ReactDOM.findDOMNode(this.refs.sliderWrapper).offsetWidth,
+      imagesLength: this.props.images.length - 1})
+  }
+
   onPrev() {
-    let prevIndex = (this.state.activeIndex - 1 >= 0) ? this.state.activeIndex - 1 : 0
+    console.log('*********PREV')
+    let prevIndex = (this.state.activeIndex - 1 >= 0)
+      ? this.state.activeIndex - 1
+      : 0
     this.setState({
       activeIndex: prevIndex,
       lazyLoadedArray: this.concatToLazyLoadedArray(prevIndex)
@@ -38,9 +53,10 @@ class Slider extends React.Component {
   }
 
   onNext() {
-    let nextIndex = (this.state.activeIndex + 1 <= this.props.data.images.length - 1)
+    console.log('*********NEXT')
+    let nextIndex = (this.state.activeIndex + 1 <= this.state.imagesLength)
       ? this.state.activeIndex + 1
-      : this.props.data.images.length -1
+      : this.state.imagesLength
     this.setState({
       activeIndex: nextIndex,
       lazyLoadedArray: this.concatToLazyLoadedArray(nextIndex)
@@ -52,24 +68,47 @@ class Slider extends React.Component {
     return (lazyLoaded.indexOf(index) === -1) ? lazyLoaded.concat(index) : lazyLoaded
   }
 
+  onSwiping(e, dX, dY, absX, absY, v) {
+    console.log(dX)
+    // first slide, don't swipe left
+    if (dX < 0 && this.state.activeIndex === 0) {
+      this.setState({dX: 0})
+      console.log('firstSlide')
+      return true
+    }
+
+    // last slide, don't swipe right
+    if (dX > 0 && this.state.activeIndex === this.state.imagesLength) {
+      this.setState({dX: 0})
+      console.log('lastSlide')
+      return true
+    }
+
+    // look for a swipe and then prev/next
+    if (Math.abs(dX > this.state.sliderWrapperWidth * 0.35)) {
+      this.setState({dX: 0})
+      dX > 0 ? this.onNext() : this.onPrev()
+    }
+    //console.log(e, dX, dY, absX, absY, v);
+  }
+
   render() {
     const {data} = this.props
     return (
-      <div className="slider-wrapper">
+      <Swipeable className="slider-wrapper" onSwiping={this.onSwiping} ref="sliderWrapper">
         <div className="prev" onClick={() => this.onPrev()}>-</div>
-        <div className="slider" style={{left: -(this.state.activeIndex * 100) + '%', transition: 'left ' + (data.slideInterval / 1000) + 's'}}>
+        <div className="slider" style={{left: -((this.state.activeIndex * this.state.sliderWrapperWidth) + this.state.dX) + 'px'}}>
           {data.images.map((image, index) =>
             <div className="slide" key={'image_'+index}>
               <div key={'slide_'+index}
                    className="slide-inner"
-                   data-image={image}
                    style={{backgroundImage: 'url(' + ((this.state.lazyLoadedArray.indexOf(index) === -1) ? '/src/img/spinner.gif' : image) + ')'}}
               />
             </div>
           )}
         </div>
         <div className="next" onClick={() => this.onNext()}>+</div>
-      </div>
+      </Swipeable>
     )
   }
 }
